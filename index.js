caf2164f2020d8c4749d73b602f881ac23896b04
Anachebe Asomugha <atasomugha@gmail.com>
2023-01-29 23:29:05 -0500

    week 4 assignment pets r us

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
     Date: 29 January 2023
     Modified By: Anachebe Asomugha
     Description: Index file for WEB 340 
-                 Assignment 4.2 - Pets-R-Us, Part 1
+                 Pets-R-Us Assignment
 ================================================                                  
  */
 // Import statements
@@ -15,14 +15,17 @@ const path = require('path');
 // Express app variable
 const app = express();
 
+// View engine set to ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// public directory path with images and stylesheets
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Listen on port 3000
+// sets listening port to 3000
 const PORT = process.env.PORT || 3000;
 
+// renders home page
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Pets-R-Us: Home',
@@ -30,7 +33,7 @@ app.get('/', (req, res) => {
     })
 });
 
-
+//renders grooming page
 app.get('/grooming', (req, res) => {
     res.render('grooming', {
         title: 'Pets-R-Us: Grooming',
@@ -38,7 +41,7 @@ app.get('/grooming', (req, res) => {
     })
 });
 
-
+// Listen on port 3000
 app.listen(PORT, () => {
     console.log('Application started and listening on PORT ' + PORT);
 });
